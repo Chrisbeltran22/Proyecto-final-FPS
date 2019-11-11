76ae4217022786941b6c64be5ad34272383b4551
Chris Beltrán <chrisgonza0@gmail.com>
2019-11-11 14:30:07 -0600

    Se modificó el menú hasta la opción de selección de tipo de auto y su digitación de placa, falta guardar su placa

--- Proyecto Parqueo.cpp
+++ Proyecto Parqueo.cpp
@@ -1,38 +1,60 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-int main (){
+int vehiculo;
+int espacio;
+
+int automovil(){
+	int placa;
+    cout<<"Ingrese numero de placa de su vehiculo:"<<endl;
+	cin>>placa;
 	
-    int consola=1;
-    int placa;
-    int vehiculo;
-    int lugar = 0;
-
-
-while(consola != 0){
-	 cout<<"Bienvenido a Super Walmart"<<endl;
-	 cout<<"Ingrese numero de placa de su vehiculo\n";
-	 cin>>placa;
-	 cout<<"1- Continuar"<<endl<<"0- Cancelar"<<endl;
-	 cin>>consola;
-	 	if (consola == 1){
-        	cout<<"Seleccione su tipo de vehiculo\n";
-         	cout<<"1- Automovil"<<endl<<"2- Motocicleta / Bicicleta"<<endl<<"0- Cancelar\n";
-         	cin>>vehiculo;
-			         
-	        if (vehiculo == 1){
-	        	cout<<"Seleccione su lugar de estacionamiento\nMostrando mapa de estacionamiento"<<endl;
-	        	cout<<"Lugares disponibles se denotan con letra D"<<endl<<"Lugares reservados se denotan con letra R"<<endl;
-	        	cin>>lugar;
-	        }
-	        if (vehiculo == 2){
-	        	cout<<"Seleccione su lugar de estacionamiento\nMostrando mapa de estacionamiento"<<endl;
-	        	cout<<"Lugares disponibles se denotan con letra D"<<endl<<"Lugares reservados se denotan con letra R"<<endl;
-	        	cin>>lugar;
-			}
-			return 0;		
-		 }
-	}
-	cout<<"La operacion ha sido cancelada"<<endl;
+	return 0;
 }
+
+int bicimoto(){
+    int placa;
+    cout<<"Ingrese numero de placa de su motocicleta o bicicleta:"<<endl;
+	cin>>placa;
+	return 0; 
+}
+
+int lugar(int automovil){
+	cout<<"Seleccione su lugar de estacionamiento\nMostrando mapa de estacionamiento"<<endl;
+    cout<<"Lugares disponibles se denotan con letra D"<<endl<<"Lugares ocupados se denotan con letra R"<<endl;
+    cin>>espacio;
+	return 0;
+}
+
+int main (){
+	int programa=0;
+	while(programa!=3){
+		time_t now = time(0);
+		tm* time = localtime(&now);
+		cout<<"Bienvenido"<<endl;
+		cout<<"Son las "<<time->tm_hour<<":"<<time->tm_min<<endl;	
+		cout<<"Seleccione su tipo de vehiculo por favor:"<<endl; 
+		cout<<"1- Automovil o Camion"<<endl<<"2- Motocicleta / Bicicleta"<<endl<<"3- Cancelar\n";
+	    cin>>vehiculo;
+	    switch(vehiculo){
+	    	case 1:
+	    		automovil();
+	    		break;
+	    	case 2:
+	    		bicimoto();
+	    		break;
+	    	case 3:
+	    		cout<<"Operacion cancelada"<<endl;
+	    		cout<<"REINICIANDO"<<endl;
+				cout<<endl;
+				cout<<endl;
+	    		break;
+	    }
+	}
+	return 0;
+}
+
+
+
